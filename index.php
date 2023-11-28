3e63bc5bf63af1e4db619e3f7afbadbfdfd707a4
FraBona <francescobonafede08@gmail.com>
2023-11-28 15:55:08 +0100

    function costruct

--- index.php
+++ index.php
@@ -16,19 +16,25 @@ class Production{
   public function getRating(){
     return $this->rating;
   }
+
+  function __construct($_title, $_language, $_rating){
+    $this->title = $_title;
+    $this->language = $_language;
+    $this->setRating($_rating);
+  }
 }
 
-$firstFilm = new Production();
-$firstFilm->title = 'Rocky';
+$firstFilm = new Production('Rocky','Italiano',4);
+/*$firstFilm->title = 'Rocky';
 $firstFilm->language = 'italiano';
-$firstFilm->setRating(4);
+$firstFilm->setRating(4);*/
 
 echo 'Il film si chiama: '.$firstFilm->title.'<br> la lingua é: '.$firstFilm->language.'<br> con valutazione di: '.$firstFilm->getRating().'<br>';
 
-$secondFilm = new Production();
-$secondFilm->title = 'Stranger things';
+$secondFilm = new Production('Stranger Things','francese',5);
+/*$secondFilm->title = 'Stranger things';
 $secondFilm->language = 'inglese';
-$secondFilm->setRating(2);
+$secondFilm->setRating(2);*/
 
 echo 'Il film si chiama: '.$secondFilm->title.'<br> la lingua é: '.$secondFilm->language.'<br> con valutazione di: '.$secondFilm->getRating();
 
